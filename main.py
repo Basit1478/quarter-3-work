7a75dded53e6e99e201abd8a82ae01d428a43770
Basit ali <ba876943@gmail.com>
2025-02-26 15:54:29 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+# First Class Hello World
+print("Hello, World!")
+
+# -------------------------------
+
+# Pycache Example
+def greet(name):
+    return f"Hello, {name}!"
+
+print(greet("Basit Ali"))
+
+# -------------------------------
+
+# data types
+
+# string
+name = "Basit Ali"
+
+# integer
+age = 20
+
+# float
+height = 5.8
+
+# boolean
+is_student = True
+
+# list
+grades = [95, 83, 90, 87, 74]
+
+# tuple
+coordinates = (3, 5)
+
+# dictionary
+person = {"name": "Basit Ali", "age": 20, "height": 5.8}
+
+# set
+unique_numbers = {1, 2, 3, 4, 5}
+
+print(name, age, height, is_student, grades, coordinates, person, unique_numbers)
