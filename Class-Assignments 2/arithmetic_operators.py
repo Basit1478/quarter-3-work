3618e87cbf8466836b9bb45d61a105d153056b6c
Basit ali <160397104+Basit1478@users.noreply.github.com>
2025-03-06 16:03:59 +0500

    Add files via upload

---
+++ Class-Assignments 2/arithmetic_operators.py
@@ -0,0 +1,38 @@
+#Arithmetic Operators
+
+#Addition
+a = 10
+b = 5
+print(a + b)
+
+#Subtraction
+a = 10
+b = 5
+print(a - b)
+
+#Multiplication
+a = 10
+b = 5
+print(a * b)
+
+#Division
+a = 10
+b = 5
+print(a / b)
+
+#Modulus
+a = 10
+b = 5
+print(a % b)
+
+#Exponentiation
+a = 10
+b = 5
+print(a ** b)
+
+#Floor Division
+a = 10
+b = 5
+print(a // b)
+
+
